00f4d3f0bde06835a0b3370e3b9abe75320c3058
Jherson <Jherson.chambi@ucb.edu.bo>
2024-11-07 01:53:17 -0400

    No pasa la prueba al sumar los numeros FALLA

--- cypress/e2e/promediar.cy.js
+++ cypress/e2e/promediar.cy.js
@@ -6,17 +6,23 @@ describe ("Promedio",()=>{
     //     cy.get("#promedio-div").should("contain", "2");
         
     // });
-    // it("Probar si suma un numero ejemplo 0 ",()=>{
-    //     cy.visit("/");
-    //     cy.get("#cadena-numeros").type("0");
-    //     cy.get("#promedio-button").click(); 
-    //     cy.get("#promedio-div").should("contain","0");
-    // });
+    it("Probar si suma un numero ejemplo 0 ",()=>{
+        cy.visit("/");
+        cy.get("#cadena-numeros").type("0");
+        cy.get("#promedio-button").click(); 
+        cy.get("#promedio-div").should("contain","0");
+    });
     it("Probar si devuelve un numero ejemplo 2 ",()=>{
         cy.visit("/");  
         cy.get("#cadena-numeros").type("2");
         cy.get("#promedio-button").click();
         cy.get("#promedio-div").should("contain","2");
+    });
+    it("Probar si suma una cadena de numeros",()=>{
+        cy.visit("/");  
+        cy.get("#cadena-numeros").type("2,2,2");
+        cy.get("#promedio-button").click();
+        cy.get("#promedio-div").should("contain","6")
     });
    
 });
--- src/promedio.js
+++ src/promedio.js
@@ -1,13 +1,18 @@
 
 function promedio(cant) {
-     let totalAcumulado = 0;
-
-    // let numeroMaximo=1000;
-
-    // let contador =0;
+    let totalAcumulado = 0;
+    let numeroMaximo=1000;
+    let contador = cant.length;
   
-     
-    return cant[0];
+    for (let i = 0; i < cant.length; i++) {
+      totalAcumulado = cant[i];
+
+  
+    }
+  
+     let total=totalAcumulado ;
+     return total;
    
   }
   export default promedio ;
+    
