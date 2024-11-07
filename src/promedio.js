dec67869a2925379f004820c95c7bc42122a09e7
Jherson <Jherson.chambi@ucb.edu.bo>
2024-11-07 03:05:34 -0400

    Probar que una serie de numeros sume pero menores de 1000 PASA

--- src/promedio.js
+++ src/promedio.js
@@ -2,16 +2,19 @@
 function promedio(cant) {
     let totalAcumulado = 0;
     let numeroMaximo=1000;
-    let contador = cant.length;
+    let contador =0;
   
     for (let i = 0; i < cant.length; i++) {
-      totalAcumulado += cant[i];
+      
 
       if (cant[i]<=numeroMaximo)
       {
         totalAcumulado += cant[i];
+        contador++;
       }
-  
+    
+      
+
     }
   
     return totalAcumulado ;
