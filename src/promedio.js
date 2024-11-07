23f9a6b62c7d36e65c5f8bc1585e2030dbac3a57
Jherson <Jherson.chambi@ucb.edu.bo>
2024-11-07 01:10:47 -0400

    Probar si el cero devuelve Falla

--- src/promedio.js
+++ src/promedio.js
@@ -1,11 +1,12 @@
 
 function promedio(cant) {
     let totalAcumulado = 0;
+
     let numeroMaximo=1000;
+    
     let contador =0;
   
     totalAcumulado=cant;
    
   }
   export default promedio ;
-    
