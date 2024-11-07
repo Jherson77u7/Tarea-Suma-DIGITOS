1e67564abaa4e5fecc699d5067ef5ee12a1b846c
Jherson <Jherson.chambi@ucb.edu.bo>
2024-11-06 23:38:34 -0400

    Suma de digitos pero solo numeros menores a 1000 falla

--- src/promedio.js
+++ src/promedio.js
@@ -1,11 +1,17 @@
 
 function promedio(cant) {
     let totalAcumulado = 0;
+    let numeroMaximo=1000;
     let contador = cant.length;
   
     for (let i = 0; i < cant.length; i++) {
       totalAcumulado += cant[i];
 
+      if (cant[i]<=numeroMaximo)
+      {
+        totalAcumulado += cant[i];
+      }
+  
     }
   
     return totalAcumulado ;
--- src/promedio.spec.js
+++ src/promedio.spec.js
@@ -1,11 +1,16 @@
 import promedio from "./promedio";
-
+//El promediador es mi fuente  de sumador de digitos
 describe ("Promedio",()=>{
 
    
     it("Primero debe sumer mis numeros",()=>{
 
         expect (promedio([1,2,4,8])).toEqual(15);
+
+    });
+  
+    it ("Numero suma solo numeros menores a 1000",()=>{
+        expect (promedio([1001,20.1004,20])).toEqual(40);
 
     });
 
